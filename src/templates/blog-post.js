8062e11dabf572df2f961af0b0011901366b0a1a
Jennifer Pinto <jennifer.pinto@rea-group.com>
2019-01-04 11:51:38 +1100

    add comments using disqus

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -6,12 +6,19 @@ import Layout from '../components/Layout'
 import SEO from '../components/seo'
 import { rhythm, scale } from '../utils/typography'
 import image from '../components/background.jpg'
+import Disqus from 'disqus-react'
 
 class BlogPostTemplate extends React.Component {
   render() {
     const post = this.props.data.markdownRemark
     const siteTitle = this.props.data.site.siteMetadata.title
-    const { previous, next } = this.props.pageContext
+    const { previous, next, slug } = this.props.pageContext
+    const disqusShortname = 'theeconomicsocio'
+    const disqusConfig = {
+      url: '',
+      identifier: slug,
+      title: post.frontmatter.title,
+    }
 
     return (
       <Layout location={this.props.location}>
@@ -61,6 +68,7 @@ class BlogPostTemplate extends React.Component {
             )}
           </li>
         </ul>
+        <Disqus.DiscussionEmbed shortname="a" config={disqusConfig} />
       </Layout>
     )
   }
